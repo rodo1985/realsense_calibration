83b7da5dc492531f587638250a0225fe336eb4ca
BCNVISION\sergio.redondo <sergio.redondo@bcnvisione.es>
2022-01-25 21:16:40 +0100

    cambios en la libreria

--- calculo_transformada.py
+++ calculo_transformada.py
@@ -1,7 +1,8 @@
 from scipy.spatial.transform import Rotation as R
 from modules.realsense import realsense
 import numpy as np
-
+import open3d as o3d
+import copy
 
 point = np.array([111, 45, 318])
 
@@ -17,9 +18,35 @@ rot_cam_flange = R.from_matrix([
 t_base_flange = np.array([-204.04, -294.36, 352.13,])
 rot_base_flange = R.from_euler('xyz', [ -179.98, -0.01, -90.42], degrees=True)
 
-
 object_to_base = t_base_flange + rot_base_flange.apply(t_cam_flange +
                                                        rot_cam_flange.apply(point, inverse=True),
                                                        inverse=False)
 
 print(object_to_base)
+
+
+# intento de visualizar fail
+# # base robot
+# robot_base_frame = o3d.geometry.TriangleMesh.create_coordinate_frame()
+# mesh_box = o3d.geometry.TriangleMesh.create_box(width=0.5,
+#                                                 height=0.5,
+#                                                 depth=0.5)
+
+# # flange
+# robot_flange_frame = copy.deepcopy(robot_base_frame)
+# R = robot_base_frame.get_rotation_matrix_from_xyz(rot_base_flange.as_euler('xyz', degrees=False))
+# robot_flange_frame.rotate(R, center=(0, 0, 0))
+# robot_flange_frame.translate(t_base_flange/100)
+
+# #camara
+# camera_frame = copy.deepcopy(robot_flange_frame)
+# R = robot_base_frame.get_rotation_matrix_from_xyz(rot_cam_flange.as_euler('xyz', degrees=False))
+# camera_frame.rotate(R, center=(0, 0, 0))
+# camera_frame.translate(t_cam_flange/100)
+
+# # objeto
+# object_frame = copy.deepcopy(camera_frame).translate(point/100 *-1)
+
+# # visualizar
+# o3d.visualization.draw_geometries([robot_base_frame, robot_flange_frame, camera_frame, object_frame, mesh_box])
+
