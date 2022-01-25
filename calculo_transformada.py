f83bdf6ee31cd592d7fd51d729912d318dc349c1
BCNVISION\sergio.redondo <sergio.redondo@bcnvisione.es>
2022-01-25 09:08:12 +0100

    test_ur_ok

--- calculo_transformada.py
+++ calculo_transformada.py
@@ -2,29 +2,20 @@ from scipy.spatial.transform import Rotation as R
 from modules.realsense import realsense
 import numpy as np
 
-# init realsense
-# rs = realsense()
 
-point = np.array([48, 267, 1062])
+point = np.array([111, 45, 318])
 
-# point = np.array([-6.001, -3.778, 470.712])
 
 # calibration
-t_cam_flange = np.array([5.68945, -18.2781, 178.771])
+t_cam_flange = np.array([13.0373, 72.0783, 37.6219])
 rot_cam_flange = R.from_matrix([
-    [-0.0730971, -0.997282,	-0.00926262],
-    [0.997322, -0.073072, -0.00302439],
-    [0.00233933, -0.00945889, 0.999953]])
-# [ x: 0.165205, y: -0.5472205, z: 91.6236921 ]
-
-# # robot pose
-# t_base_flange = np.array([196.53,-1534.6, 1156.53])
-# rot_base_flange = R.from_euler('zyx', [-30.68, 0.61, 178.91], degrees=True)
+    [-0.999883, 0.00485597,	0.0145292],
+    [-0.00642179, -0.993939, -0.109744],
+    [0.0139082, -0.109825, 0.993854]])
 
 # robot pose
-t_base_flange = np.array([507.34, 1223.44, 1545.22])
-
-rot_base_flange = R.from_euler('zyx', [-32.46, 0, -180], degrees=True)
+t_base_flange = np.array([-204.04, -294.36, 352.13,])
+rot_base_flange = R.from_euler('xyz', [ -179.98, -0.01, -90.42], degrees=True)
 
 
 object_to_base = t_base_flange + rot_base_flange.apply(t_cam_flange +
--- kukaCalibration.py
+++ kukaCalibration.py
@@ -9,9 +9,9 @@ from scipy.spatial.transform import Rotation as R
 
 def draw(img, corners, imgpts):
     corner = tuple(corners[0].ravel())
-    # img = cv2.line(img, corner, tuple(imgpts[0].ravel()), (255, 0, 0), 5)
-    # img = cv2.line(img, corner, tuple(imgpts[1].ravel()), (0, 255, 0), 5)
-    # img = cv2.line(img, corner, tuple(imgpts[2].ravel()), (0, 0, 255), 5)
+    img = cv2.line(img, corner, tuple(imgpts[0].ravel()), (255, 0, 0), 5)
+    img = cv2.line(img, corner, tuple(imgpts[1].ravel()), (0, 255, 0), 5)
+    img = cv2.line(img, corner, tuple(imgpts[2].ravel()), (0, 0, 255), 5)
     return img
 
 
@@ -99,6 +99,7 @@ def main():
     # Chesboard
     nx = 11
     ny = 8
+    first_image = True
 
 
     # Constants
@@ -108,8 +109,6 @@ def main():
     IP = "192.168.101.145"
     # IP = "localhost"
     Port = 6000
-
-
 
     # Create a pipeline
     pipeline = rs.pipeline()
@@ -145,106 +144,112 @@ def main():
     # Streaming loop
     try:
         while True:
-
+            
             # Get frameset of color and depth
             frames = pipeline.wait_for_frames()
-            # frames.get_depth_frame() is a 640x360 depth image
 
-            # Align the depth frame to color frame
-            aligned_frames = align.process(frames)
+            if not first_image:
 
-            # Get aligned frames
-            aligned_depth_frame = aligned_frames.get_depth_frame()  # aligned_depth_frame is a 640x480 depth image
-            color_frame = aligned_frames.get_color_frame()
+                # Align the depth frame to color frame
+                aligned_frames = align.process(frames)
 
-            # Validate that both frames are valid
-            if not aligned_depth_frame or not color_frame:
-                continue
+                # Get aligned frames
+                aligned_depth_frame = aligned_frames.get_depth_frame()  # aligned_depth_frame is a 640x480 depth image
+                color_frame = aligned_frames.get_color_frame()
 
-            color_image = np.asanyarray(color_frame.get_data())
+                # Validate that both frames are valid
+                if not aligned_depth_frame or not color_frame:
+                    continue
 
-            bg_removed = color_image
+                color_image = np.asanyarray(color_frame.get_data())
 
-            ret, img, points, rvects = checkerboard_calibration(bg_removed, nx, ny)
+                bg_removed = color_image
 
-            coord = []
+                ret, img, points, rvects = checkerboard_calibration(bg_removed, nx, ny)
 
-            if ret:
-                if EnableTcpServer:
-                    # Esperamos la pose del robot
-                    robot_pose = tcp.waitForPose()
-                    robot_pose = np.array(robot_pose.split(','), float)
-                    robot_pose[0:3] = robot_pose[0:3] * 1000
-                    robot_poses.append([robot_pose[0], robot_pose[1], robot_pose[2], robot_pose[3],robot_pose[4],robot_pose[5]])
+                coord = []
 
-                for i in range(nx * ny):
+                if ret:
+                    if EnableTcpServer:
+                        # Esperamos la pose del robot
+                        robot_pose = tcp.waitForPose()
+                        robot_pose = np.array(robot_pose.split(','), float)
+                        robot_pose[0:3] = robot_pose[0:3] * 1000
+                        robot_poses.append([robot_pose[0], robot_pose[1], robot_pose[2], robot_pose[3],robot_pose[4],robot_pose[5]])
 
-                    x, y, z = convert_depth_pixel_to_metric_coordinate(aligned_depth_frame, points[0][i][0][0],
-                                                                       points[0][i][0][1])
-                    point = np.array((x, y, z))
-                    if point[0] != 0 and point[1] != 0 and point[2] != 0:
-                        coord.append(point)
+                    for i in range(nx * ny):
 
-                plane = Plane.best_fit(np.array(coord))
-                rot_x = math.acos(plane.normal[0]) + math.pi/2
-                rot_y = math.acos(plane.normal[1]) - math.pi/2
-                rot_z = math.pi / 2 + rvects[2][0]
+                        x, y, z = convert_depth_pixel_to_metric_coordinate(aligned_depth_frame, points[0][i][0][0],
+                                                                        points[0][i][0][1])
+                        point = np.array((x, y, z))
+                        if point[0] != 0 and point[1] != 0 and point[2] != 0:
+                            coord.append(point)
 
-
-                euler = R.from_euler('xyz', [rot_x, rot_y, rot_z], degrees=False)
-                quaternion = euler.as_quat()
-
-                if hand_eye:
-                    transform = np.eye(3)
-                    transform[:3, :3] = euler.inv().as_matrix()
-                    p = np.array([coord[0][0], coord[0][1], coord[0][2]]) * -1
-                    point = np.dot(p, transform)
-
-                    camposes.append([point[0], point[1], point[2], quaternion[3], quaternion[0], quaternion[1], quaternion[2]
-                                     ])
-                else:
-                    camposes.append([coord[0][0], coord[0][1], coord[0][2], quaternion[3], quaternion[0], quaternion[1], quaternion[2]
-                                 ])
-
-                if not EnableTcpServer:
-                    print(camposes[len(camposes)-1])
-
-                bg_removed = img
+                    plane = Plane.best_fit(np.array(coord))
+                    rot_x = math.acos(plane.normal[0]) + math.pi/2
+                    rot_y = math.acos(plane.normal[1]) - math.pi/2
+                    rot_z = math.pi / 2 + rvects[2][0]
 
 
+                    euler = R.from_euler('xyz', [rot_x, rot_y, rot_z], degrees=False)
+                    quaternion = euler.as_quat()
 
-            cv2.namedWindow('Align Example', cv2.WINDOW_AUTOSIZE)
-            cv2.imshow('Align Example', bg_removed)
-            key = cv2.waitKey()
+                    if hand_eye:
+                        
+                        transform = np.eye(3)
+                        transform[:3, :3] = euler.inv().as_matrix()
 
-            # # Press esc or 'q' to close the image window
-            if key & 0xFF == ord('q') or key == 27:
-                if EnableTcpServer:
-                    # # Guardamos las coordenadas
-                    # np.savetxt('test.txt', camposes, delimiter=',')  # X is an array
-                    for i in range(len(camposes)):
-                        print(str(robot_poses[i][0]) + ' ' +
-                              str(robot_poses[i][1]) + ' ' +
-                              str(robot_poses[i][2]) + ' ' +
-                              str(robot_poses[i][3]) + ' ' +
-                              str(robot_poses[i][4]) + ' ' +
-                              str(robot_poses[i][5]) + ' ' +
-                              str(camposes[i][0]) + ' ' +
-                              str(camposes[i][1]) + ' ' +
-                              str(camposes[i][2]) + ' ' +
-                              str(camposes[i][3]) + ' ' +
-                              str(camposes[i][4]) + ' ' +
-                              str(camposes[i][5]) + ' ' +
-                              str(camposes[i][6])
-                              )
+                        p = np.array([coord[0][0], coord[0][1], coord[0][2]]) * -1
+
+                        transformed_point = np.dot(p, transform)
+
+                        camposes.append([transformed_point[0], transformed_point[1], transformed_point[2], quaternion[3], quaternion[0], quaternion[1], quaternion[2]
+                                        ])
+                    else:
+                        camposes.append([coord[0][0], coord[0][1], coord[0][2], quaternion[3], quaternion[0], quaternion[1], quaternion[2]
+                                    ])
+
+                    if not EnableTcpServer:
+                        print(camposes[len(camposes)-1])
+
+                    bg_removed = img
 
 
-                if EnableTcpServer:
-                    tcp.close()
-                    del tcp
 
-                cv2.destroyAllWindows()
-                break
+                cv2.namedWindow('Align Example', cv2.WINDOW_AUTOSIZE)
+                cv2.imshow('Align Example', bg_removed)
+                key = cv2.waitKey()
+
+                # # Press esc or 'q' to close the image window
+                if key & 0xFF == ord('q') or key == 27:
+                    if EnableTcpServer:
+                        # # Guardamos las coordenadas
+                        # np.savetxt('test.txt', camposes, delimiter=',')  # X is an array
+                        for i in range(len(camposes)):
+                            print(str(robot_poses[i][0]) + ' ' +
+                                str(robot_poses[i][1]) + ' ' +
+                                str(robot_poses[i][2]) + ' ' +
+                                str(robot_poses[i][3]) + ' ' +
+                                str(robot_poses[i][4]) + ' ' +
+                                str(robot_poses[i][5]) + ' ' +
+                                str(camposes[i][0]) + ' ' +
+                                str(camposes[i][1]) + ' ' +
+                                str(camposes[i][2]) + ' ' +
+                                str(camposes[i][3]) + ' ' +
+                                str(camposes[i][4]) + ' ' +
+                                str(camposes[i][5]) + ' ' +
+                                str(camposes[i][6])
+                                )
+
+
+                    if EnableTcpServer:
+                        tcp.close()
+                        del tcp
+
+                    cv2.destroyAllWindows()
+                    break
+            else:
+                first_image = False
     finally:
         pipeline.stop()
 
